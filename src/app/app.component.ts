ea29863e1c80cb852386da1de12b0c9aaebd19ad
David Seva <dseva1975@gmail.com>
2022-10-24 00:16:27 +0200

    App-empleados. Directivas III - Video 16

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Empleado } from './empleado.model';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +7,13 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app-empleados';
+  title = 'Listado de Empleados';
+
+  empleados:Empleado[]=[ //array de empleados
+  new Empleado("David","Seva","Presidente",8500),
+  new Empleado("Maria","Gomez","Administrativa",2500),
+  new Empleado("Laura","Martínez","Directora",5500),
+  new Empleado("Violeta","Mas","Limpiadora",600)
+
+  ] 
 }
