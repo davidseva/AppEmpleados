70a05f7d7c3048fc09e6b9e3af7e8224c03f2936
David Seva <dseva1975@gmail.com>
2022-12-06 19:50:17 +0100

    Routing V - Video 28 - Actualizar empleados

--- src/app/actualiza-component/actualiza-component.component.ts
+++ src/app/actualiza-component/actualiza-component.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { Router } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Empleado } from '../empleado.model';
 import { EmpleadosServices } from '../empleados.service';
 import { ServicioEmpleadosService } from '../servicio-empleados.service';
@@ -11,10 +11,16 @@ import { ServicioEmpleadosService } from '../servicio-empleados.service';
 })
 export class ActualizaComponentComponent implements OnInit {
 
-  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosServices) { }
+  constructor(private router:Router, private miServicio:ServicioEmpleadosService, private empleadosService:EmpleadosServices, private route:ActivatedRoute) { }
 
   ngOnInit(): void {
     this.empleados = this.empleadosService.empleados;
+    this.indice = this.route.snapshot.params['id'];
+    let empleado:Empleado = this.empleadosService.encontrarEmpleado(this.indice);
+    this.cuadroNombre = empleado.nombre;
+    this.cuadroApellido = empleado.apellido;
+    this.cuadroCargo = empleado.cargo;
+    this.cuadroSalario = empleado.salario;
   }
 
   empleados:Empleado[]=[]; //array de empleados
@@ -24,10 +30,10 @@ export class ActualizaComponentComponent implements OnInit {
     this.router.navigate(['']);
   }
 
-  agregarEmpleado(){
+  actualizaEmpleado(){
     let miEmpleado=new Empleado(this.cuadroNombre, this.cuadroApellido, this.cuadroCargo, this.cuadroSalario)
     //this.miServicio.muestraMensaje("Nombre del empleado: " + miEmpleado.nombre);
-    this.empleadosService.agregarEmpleadoServicio(miEmpleado);
+    this.empleadosService.actualizarEmpleado(this.indice, miEmpleado);
     
     this.router.navigate(['']);
   }
@@ -36,5 +42,6 @@ export class ActualizaComponentComponent implements OnInit {
   cuadroApellido:string="";
   cuadroCargo:string="";
   cuadroSalario:number=0;
+  indice:number=0;
 
 }
