d2d35b3c186d86fff7e4ca0a0a3b53dc3d51554e
David Seva <dseva1975@gmail.com>
2022-12-08 12:13:38 +0100

    Routing VII - 31 - Rutas de error personalizadas

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -13,13 +13,15 @@ import { QuienesComponentComponent } from './quienes-component/quienes-component
 import { ContactoComponentComponent } from './contacto-component/contacto-component.component';
 import { Routes, RouterModule } from '@angular/router';
 import { ActualizaComponentComponent } from './actualiza-component/actualiza-component.component';
+import { ErrorPersonalizadoComponent } from './error-personalizado/error-personalizado.component';
 
 const appRoutes:Routes=[
   {path:'', component:HomeComponentComponent},
   {path:'proyectos', component:ProyectosComponentComponent},
   {path:'quienes', component:QuienesComponentComponent},
   {path:'contacto', component:ContactoComponentComponent},
-  {path:'actualiza/:id', component:ActualizaComponentComponent}
+  {path:'actualiza/:id', component:ActualizaComponentComponent},
+  {path:'**', component:ErrorPersonalizadoComponent}
 
 
 
@@ -34,7 +36,8 @@ const appRoutes:Routes=[
     ProyectosComponentComponent,
     QuienesComponentComponent,
     ContactoComponentComponent,
-    ActualizaComponentComponent
+    ActualizaComponentComponent,
+    ErrorPersonalizadoComponent
   ],
   imports: [
     BrowserModule,
